e34c6a4aa9f0de20afd564423d00ef8ea17e7e7f
Дмитрий <HappyChel@mail.ru>
2021-05-19 01:20:40 +0500

    Добавляет страницу просмотра поста и сортировку постов

---
+++ post.php
@@ -0,0 +1,73 @@
+<?php
+require_once('helpers.php');
+
+$is_auth = 1;
+$user_name = "Дмитрий";
+
+$id = 0;
+$post = [];
+
+$id = filter_input(INPUT_GET, 'id');
+
+$db_link = mysqli_connect("127.0.0.1", "root", "root", "readme");
+if ($db_link == false) {
+    print("Ошибка подключения: " . mysqli_connect_error());
+} else {
+    mysqli_set_charset($db_link, "utf8");
+
+    if(!mysqli_query($db_link, "SELECT id FROM posts WHERE id = $id")) {
+        header("HTTP/1.0 404 Not Found");
+    }
+
+    if($id){
+        $post = make_select_query($db_link, 
+            "SELECT p.*, type_name AS type, icon_class AS class, COUNT(c.id) AS comments, COUNT(l.id) AS likes 
+            FROM posts p
+            JOIN types tp ON p.post_type = tp.id
+            LEFT JOIN comments c ON p.id = c.post_id
+            LEFT JOIN likes l ON p.id = l.post_id
+            GROUP BY p.id
+            HAVING p.id = $id;"
+        );
+        
+        $user_id = $post[0]['user_id'];
+        $user = make_select_query($db_link, 
+            "SELECT u.*, COUNT(sub.id) AS subs, COUNT(p.id) AS posts
+            FROM users u 
+            LEFT JOIN subscriptions sub ON u.id = sub.user_id
+            LEFT JOIN posts p ON u.id = p.user_id
+            GROUP BY u.id
+            HAVING u.id = $user_id;"
+        );
+
+        $comments = make_select_query($db_link, 
+        "SELECT comment, published_at, name, avatar_img 
+        FROM comments c
+        JOIN users u ON c.user_id = u.id
+        WHERE c.post_id = $id
+        ORDER BY published_at DESC LIMIT 4;"
+        );
+    }
+}
+
+$post_file = "post-" . $post[0]['class'] . ".php";
+
+$post_content = include_template($post_file, [
+    'post' => $post[0]
+    ]);
+$page_content = include_template('post-main.php', [
+    'id' => $id,
+    'content' => $post_content,
+    'post' => $post[0],
+    'user' => $user[0],
+    'comments' => $comments
+    ]);
+$layout_content = include_template('layout.php', [
+    'content' => $page_content,
+    'is_auth' => $is_auth,
+    'user_name' => $user_name,
+    'title' => 'readme: публикация'
+    ]);
+
+print($layout_content);
+?>
